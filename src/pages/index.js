c31417227a13271a24744a77712efa3f39b28342
Muhammed Saryyew <nsaryyev9797@gmail.com>
2021-11-07 20:53:14 +0200

    exported all pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,63 @@
-import Home from "./home";
+import Accordions from "./accordions";
 import Allerts from "./allerts";
-import ButtonsGroup from "./buttons-group";
+import Badge from "./badge";
+import Breadcrumbs from "./breadcrumbs";
 import Buttons from "./buttons";
+import ButtonsGroup from "./buttons-group";
+import Cards from "./cards";
+import Carousels from "./carousels";
+import CloseButtons from "./close-buttons";
+import Dropdowns from "./dropdowns";
+import Figures from "./figures";
+import Forms from "./forms";
+import Home from "./home";
+import Images from "./images";
+import InputGroups from "./input-groups";
+import ListGroups from "./list-groups";
+import Modals from "./modals";
+import Navbars from "./navbars";
+import Navs from "./navs";
+import Offcanvas from "./offcanvas";
+import Overlays from "./overlays";
+import Paginations from "./paginations";
+import Placeholders from "./placeholders";
+import Popovers from "./popovers";
+import ProgressBars from "./progress-bars";
+import Spinners from "./spinners";
+import TabbedComponents from "./tabbed-components";
+import Tables from "./tables";
+import Toasts from "./toasts";
+import Tooltips from "./tooltips";
 
-export { Home, Allerts, ButtonsGroup, Buttons };
+export {
+  Accordions,
+  Allerts,
+  Badge,
+  Breadcrumbs,
+  Buttons,
+  ButtonsGroup,
+  Cards,
+  Carousels,
+  CloseButtons,
+  Dropdowns,
+  Figures,
+  Forms,
+  Home,
+  Images,
+  InputGroups,
+  ListGroups,
+  Modals,
+  Navbars,
+  Navs,
+  Offcanvas,
+  Overlays,
+  Paginations,
+  Placeholders,
+  Popovers,
+  ProgressBars,
+  Spinners,
+  TabbedComponents,
+  Tables,
+  Toasts,
+  Tooltips,
+};
